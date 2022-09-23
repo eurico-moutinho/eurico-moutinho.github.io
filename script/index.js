2ef1f6ffad49fa4610ee5c2be6f4c5dc9353fdee
Eurico Moutinho <yungsniper@hotmail>
2022-09-23 16:33:24 +0100

    Profile

--- script/index.js
+++ script/index.js
@@ -5,20 +5,32 @@ const btnc = document.getElementById('contacts');
 const secta = document.getElementById('about');
 const sectp = document.getElementById('project');
 const sectc = document.getElementById('contact');
+const land = document.getElementById('landing');
+const btnland = document.getElementById('land');
 const nav = document.getElementById('navbar-toggler');
 const bar = document.getElementById('navbarNav');
 window.onload = () => {
-    if (sectp && sectc && secta) {
+    if (sectp && sectc && secta && land) {
         sectp.style.display = 'none';
         sectc.style.display = 'none';
-        secta.style.display = 'inline';
+        secta.style.display = 'none';
+        land.style.display = 'inline';
     }
 };
+if (btnland) {
+    btnland.onclick = () => {
+        if (sectp && land) {
+            land.style.display = 'none';
+            sectp.style.display = 'inline';
+        }
+    };
+}
 if (btna) {
     btna.onclick = () => {
-        if (sectp && sectc && secta && nav && bar) {
+        if (sectp && sectc && secta && nav && bar && land) {
             nav.classList.add('collapsed');
             bar.classList.remove('show');
+            land.style.display = 'none';
             sectp.style.display = 'none';
             sectc.style.display = 'none';
             secta.style.display = 'inline';
@@ -27,9 +39,10 @@ if (btna) {
 }
 if (btnp) {
     btnp.onclick = () => {
-        if (sectp && sectc && secta && nav && bar) {
+        if (sectp && sectc && secta && nav && bar && land) {
             nav.classList.add('collapsed');
             bar.classList.remove('show');
+            land.style.display = 'none';
             secta.style.display = 'none';
             sectc.style.display = 'none';
             sectp.style.display = 'inline';
@@ -38,9 +51,10 @@ if (btnp) {
 }
 if (btnc) {
     btnc.onclick = () => {
-        if (sectp && sectc && secta && nav && bar) {
+        if (sectp && sectc && secta && nav && bar && land) {
             nav.classList.add('collapsed');
             bar.classList.remove('show');
+            land.style.display = 'none';
             secta.style.display = 'none';
             sectp.style.display = 'none';
             sectc.style.display = 'inline';
--- script/index.ts
+++ script/index.ts
@@ -4,21 +4,32 @@ const btnc = document.getElementById('contacts');
 const secta = document.getElementById('about');
 const sectp = document.getElementById('project');
 const sectc = document.getElementById('contact');
+const land = document.getElementById('landing');
+const btnland = document.getElementById('land');
 const nav = document.getElementById('navbar-toggler');
 const bar = document.getElementById('navbarNav');
 
 window.onload=()=> {
-    if(sectp && sectc && secta){
+    if(sectp && sectc && secta && land){
     sectp.style.display='none';
     sectc.style.display = 'none';
-    secta.style.display='inline';}
+    secta.style.display='none';
+    land.style.display='inline';}
 }
+
+if(btnland){
+    btnland.onclick = () => {
+        if(sectp && land){
+        land.style.display='none';
+        sectp.style.display='inline';
+    }}}
 
 if(btna){
 btna.onclick = () => {
-    if(sectp && sectc && secta && nav && bar){
+    if(sectp && sectc && secta && nav && bar && land){
     nav.classList.add('collapsed');
     bar.classList.remove('show');
+    land.style.display='none';
     sectp.style.display='none';
     sectc.style.display = 'none';
     secta.style.display='inline';
@@ -26,9 +37,10 @@ btna.onclick = () => {
 
 if(btnp){
 btnp.onclick = () => {
-    if(sectp && sectc && secta && nav && bar){
+    if(sectp && sectc && secta && nav && bar && land){
     nav.classList.add('collapsed');
     bar.classList.remove('show');
+    land.style.display='none';
     secta.style.display='none';
     sectc.style.display = 'none';
     sectp.style.display='inline';
@@ -36,9 +48,10 @@ btnp.onclick = () => {
 
 if(btnc){
 btnc.onclick = () => {
-    if(sectp && sectc && secta && nav && bar){
+    if(sectp && sectc && secta && nav && bar && land){
     nav.classList.add('collapsed');
     bar.classList.remove('show');
+    land.style.display='none';
     secta.style.display='none';
     sectp.style.display='none';
     sectc.style.display = 'inline';
