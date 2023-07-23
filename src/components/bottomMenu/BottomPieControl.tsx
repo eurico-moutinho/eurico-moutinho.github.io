cdf232396cee3ad07cd7540c9696caf974e22cf1
Eurico Moutinho <108530777+eurico-moutinho@users.noreply.github.com>
2023-07-23 20:22:20 +0100

    Redone

---
+++ src/components/bottomMenu/BottomPieControl.tsx
@@ -0,0 +1,90 @@
+import React, { useState, useEffect, useRef } from 'react'
+import { NavLink } from 'react-router-dom';
+import './BottomPieControl.sass'
+
+const BottomPieControl: React.FC = () => {
+
+    const [isExpanded, setIsExpanded] = useState<boolean>(false);
+
+    const menuRef = useRef<HTMLDivElement>(null);
+  
+    const handleControlClick = (): void => {
+
+      setIsExpanded(!isExpanded);
+
+    };
+
+    const handleClickOutside = (event: MouseEvent): void => {
+
+        if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+
+          setIsExpanded(false);
+
+        }
+      };
+
+    useEffect(() => {
+
+        document.addEventListener('click', handleClickOutside);
+
+        return () => {
+
+            document.removeEventListener('click', handleClickOutside);
+            
+        };
+    }, []);
+    
+    return (
+
+      <main className="bottomContainer laptop-hidden smallScreen-hidden desktop-hidden">
+
+        <div className="BottomPieControl flex justify-content-center align-items-center" onClick={handleControlClick}  ref={menuRef}>
+            <i className="fa-solid fa-bars fa-xl"></i>
+        </div>
+
+        <ul className="bottomMenu">
+
+            <li className={`choice flex justify-content-end ${isExpanded ? 'expanded' : ''}`}>
+                <a href="https://twitter.com/YuriYung" target="_blank" rel="noreferrer" title="Twitter" className="flex justify-content-center align-items-center" onClick={handleControlClick}>
+                    <i className="fa-brands fa-twitter fa-fw fa-xl"></i>
+                </a>                
+            </li>
+
+            <li className={`choice flex justify-content-end ${isExpanded ? 'expanded' : ''}`}>
+                <a href="https://www.linkedin.com/in/eurico-yung-moutinho/" target="_blank" rel="noreferrer" title="Linkedin" className="flex justify-content-center align-items-center" onClick={handleControlClick}>
+                    <i className="fa-brands fa-linkedin fa-fw fa-xl"></i>
+                </a>
+            </li>
+
+            <li className={`choice flex justify-content-end ${isExpanded ? 'expanded' : ''}`}>
+                <NavLink to="/" title="Home" className="flex justify-content-center align-items-center" onClick={handleControlClick}>
+                    <i className="fa-solid fa-house fa-fw fa-xl"></i>
+                </NavLink>
+            </li>
+
+            <li className={`choice flex justify-content-end ${isExpanded ? 'expanded' : ''}`}>
+                <NavLink to="/projects" title="Projects" className="flex justify-content-center align-items-center" onClick={handleControlClick}>
+                    <i className="fa-solid fa-briefcase fa-fw fa-xl"></i>
+                </NavLink>
+            </li>
+
+            <li className={`choice flex justify-content-end ${isExpanded ? 'expanded' : ''}`}>
+                <NavLink to="/about" title="About me" className="flex justify-content-center align-items-center" onClick={handleControlClick}>
+                    <i className="fa-regular fa-address-card fa-fw fa-xl"></i>
+                </NavLink>
+            </li>
+
+            <li className={`choice flex justify-content-end ${isExpanded ? 'expanded' : ''}`}>
+                <a href="https://github.com/eurico-moutinho" target="_blank" rel="noreferrer" title="Github" className="flex justify-content-center align-items-center" onClick={handleControlClick}>
+                    <i className="fa-brands fa-github fa-fw fa-xl"></i>
+                </a>
+            </li>
+
+        </ul>
+
+      </main>
+
+    );
+  }
+  
+  export default BottomPieControl;
---
+++ src/pages/projectos/Projectos.tsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import './Projectos.sass';
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+const Projectos = () => {
+
+  interface Project {
+
+    title: string;
+    year: string;
+    image: string;
+    languages: { class: string; title: string }[];
+    with: string;
+    description: string;
+    deployLink: string;
+    githubLink: string;
+
+  };
+
+  const [dataList, setData] = useState<Project[]>([]);
+
+  useEffect(() => {
+
+    axios.get<Project[]>(
+      '../../../data/projects.json',
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json"
+        }
+      }).then( r => {
+
+        setData(r.data);
+  
+      });
+
+  }, []);
+
+  return (
+    <>
+      <h1 id='projectTitle' className='my50'>Projects</h1>
+
+      <section className="cardList justify-content-center pb-xsm50">
+        {dataList.map( (e, index) => {
+
+          return (
+
+            <fieldset key={index} className="card p10 flex flex-column align-items-center">
+              <legend>{ e.title } <span>({ e.year })</span></legend>
+              <img src={ e.image } alt="projectPreview"/>
+              <div className='icons my10'>{ e.languages.map((language, i) => {
+
+                return <i key={i} className={`${language.class} fa-lg`} title={language.title}></i>
+
+              })}</div>
+              <p>{ e.with }</p>
+              <p>{ e.description }</p>
+              <div className='links my10 flex justify-content-evenly'>
+                <a target='_blank' rel="noreferrer" href={ e.deployLink } className='py5'>Page</a>
+                <a target='_blank' rel="noreferrer" href={ e.githubLink} className='py5'>GitHub</a>
+              </div>
+            </fieldset>
+
+          )
+
+        })}
+      </section>
+    </>
+  );
+}
+
+export default Projectos;
