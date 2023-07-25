40cc5839634a19756029fc0d9f5f2853b8ad83e5
Eurico Moutinho <108530777+eurico-moutinho@users.noreply.github.com>
2023-07-25 21:12:55 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.sass';
 import Home from './pages/home/Home';
 import Projectos from './pages/projectos/Projectos';
