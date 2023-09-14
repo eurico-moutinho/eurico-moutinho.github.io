2d3eb9e568d0a7d7a70e9443b4af8fe8eab7f22a
Eurico Moutinho <108530777+eurico-moutinho@users.noreply.github.com>
2023-09-14 17:46:21 +0100

    reflection

--- src/components/pieControl/PieControl.tsx
+++ src/components/pieControl/PieControl.tsx
@@ -3,57 +3,57 @@ import { NavLink } from 'react-router-dom';
 import './PieControl.sass'
 
 const PieControl: React.FC = () => {
-    return (
+  return (
 
-      <main className="pieContainer mobile-hidden tablet-hidden">
+    <main className="pieContainer mobile-hidden tablet-hidden">
 
-        <div className="pieButton flex flex-column justify-content-center align-items-center">
-            <span>M</span><span>e</span><span>n</span><span>u</span>
-        </div>
+      <div className="pieButton flex flex-column justify-content-center align-items-center">
+        <span>M</span><span>e</span><span>n</span><span>u</span>
+      </div>
 
-        <ul className="pieMenu flex flex-column justify-content-center">
+      <ul className="pieMenu flex flex-column justify-content-center">
 
-          <li className='slice flex justify-content-end'>
-            <a href="https://twitter.com/YuriYung" target="_blank" rel="noreferrer" title="Twitter" className="flex justify-content-center align-items-center">
-                <i className="fa-brands fa-x-twitter fa-fw fa-xl"></i>
-            </a>
-          </li>
+        <li className='slice flex justify-content-end'>
+          <a href="https://twitter.com/YuriYung" target="_blank" rel="noreferrer" title="X.com" className="flex justify-content-center align-items-center">
+            <i className="fa-brands fa-x-twitter fa-fw fa-xl"></i>
+          </a>
+        </li>
 
-          <li className='slice flex justify-content-end'>
-            <a href="https://www.linkedin.com/in/eurico-yung-moutinho/" target="_blank" rel="noreferrer" title="Linkedin" className="flex justify-content-center align-items-center">
-              <i className="fa-brands fa-linkedin fa-fw fa-xl"></i>
-            </a>
-          </li>
+        <li className='slice flex justify-content-end'>
+          <a href="https://www.linkedin.com/in/eurico-yung-moutinho/" target="_blank" rel="noreferrer" title="Linkedin" className="flex justify-content-center align-items-center">
+            <i className="fa-brands fa-linkedin fa-fw fa-xl"></i>
+          </a>
+        </li>
 
-          <li className='slice flex justify-content-end'>
-            <NavLink to="/" title="Home" className="flex justify-content-center align-items-center">
-              <i className="fa-solid fa-house fa-fw fa-xl"></i>
-            </NavLink>
-          </li>
+        <li className='slice flex justify-content-end'>
+          <NavLink to="/" title="Home" className="flex justify-content-center align-items-center">
+            <i className="fa-solid fa-house fa-fw fa-xl"></i>
+          </NavLink>
+        </li>
 
-          <li className='slice flex justify-content-end'>
-            <NavLink to="/projects" title="Projects" className="flex justify-content-center align-items-center">
-              <i className="fa-solid fa-briefcase fa-fw fa-xl"></i>
-            </NavLink>
-          </li>
+        <li className='slice flex justify-content-end'>
+          <NavLink to="/projects" title="Projects" className="flex justify-content-center align-items-center">
+            <i className="fa-solid fa-briefcase fa-fw fa-xl"></i>
+          </NavLink>
+        </li>
 
-          <li className='slice flex justify-content-end'>
-            <NavLink to="/about" title="About me" className="flex justify-content-center align-items-center">
-              <i className="fa-regular fa-address-card fa-fw fa-xl"></i>
-            </NavLink>
-          </li>
+        <li className='slice flex justify-content-end'>
+          <NavLink to="/about" title="About me" className="flex justify-content-center align-items-center">
+            <i className="fa-regular fa-address-card fa-fw fa-xl"></i>
+          </NavLink>
+        </li>
 
-          <li className='slice flex justify-content-end'>
-            <a href="https://github.com/eurico-moutinho" target="_blank" rel="noreferrer" title="Github" className="flex justify-content-center align-items-center">
-              <i className="fa-brands fa-github fa-fw fa-xl"></i>
-            </a>
-          </li>
+        <li className='slice flex justify-content-end'>
+          <a href="https://github.com/eurico-moutinho" target="_blank" rel="noreferrer" title="Github" className="flex justify-content-center align-items-center">
+            <i className="fa-brands fa-github fa-fw fa-xl"></i>
+          </a>
+        </li>
 
-        </ul>
+      </ul>
 
-      </main>
+    </main>
 
-    );
-  }
-  
-  export default PieControl;
+  );
+}
+
+export default PieControl;
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -37,16 +37,16 @@ const Home: React.FC = () => {
   return (
     <AnimatedPage>
       <div id="profile" className="flex flex-column justify-content-center align-items-center">
-        <h1 className="mb50 ml20 mt-xsm80 mt-sm80">Hi,<span><br/></span>welcome to my corner of the internet</h1>
+        <h1 className="mb50 ml20 mt-xsm80 mt-sm80">Hi,<span><br /></span>welcome to my corner of the internet</h1>
         <div className="flex justify-content-evenly align-items-center flex-wrap subpro">
           <div id="description">
             <code className="p30">
               <p className="typewriter"><span className="obj">Name</span> = &#123;</p>
-              <br/>
+              <br />
               <p className="variables typewriter ml15 mr80 mr-sm40 mr-xsm20"><span>Firstname:</span> 'Eurico',</p>
               <p className="variables typewriter ml15"><span>Middlename:</span> 'Yung',</p>
               <p className="variables typewriter ml15"><span>Lastname:</span> 'Moutinho'</p>
-              <br/>
+              <br />
               <p className="typewriter">&#125;;</p>
 
               <div className="mouse-position-tracker">
@@ -63,11 +63,11 @@ const Home: React.FC = () => {
             </code>
             <code className="p30 ml100 ml-sm50 ml-xsm0">
               <p className="typewriter2"><span className="obj">What_am_I</span> = &#123;</p>
-              <br/>
+              <br />
               <p className="variables typewriter2 ml15 mr80 mr-sm40 mr-xsm20"><span>Profession:</span> 'Web Developer',</p>
               <p className="variables typewriter2 ml15"><span>Nationality:</span> 'Portuguese',</p>
               <p className="variables typewriter2 ml15"><span>Education:</span> 'Master degree'</p>
-              <br/>
+              <br />
               <p className="typewriter2">&#125;;</p>
 
               <div className="mouse-position-tracker">
@@ -83,7 +83,7 @@ const Home: React.FC = () => {
               </div>
             </code>
           </div>
-          <img src="../../images/photo-no-background.png" alt="profile" className="mx50 mb50"/>
+          <img src="../../images/photo-no-background.png" alt="profile" className="mx50 mb50" />
         </div>
       </div>
     </AnimatedPage>
